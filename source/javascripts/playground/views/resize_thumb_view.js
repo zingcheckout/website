b32817f0a5a8cdd7d7c566bfa111fd8fd457e9fc
Tom Dale <tom@sproutcore.com>
2012-10-19 17:45:29 -0700

    Fix bug where iframe was swallowing mousemove

--- source/javascripts/playground/views/resize_thumb_view.js
+++ source/javascripts/playground/views/resize_thumb_view.js
@@ -6,30 +6,29 @@ App.ResizeThumb = Ember.View.extend({
     return "left: %@px".fmt(this.get('left'));
   }.property('left'),
 
+  didInsertElement: function() {
+    $('<div id="mouse-detector"></div>').appendTo(document.body);
+  },
+
   mouseDown: function(event) {
     var startX = event.clientX;
     var startResizerPos = this.get('resizerPos');
 
     var self = this;
-    $(document).on('mousemove', function(event) {
+
+    var mouseDetector = $('#mouse-detector');
+
+    mouseDetector.addClass('active');
+    mouseDetector.on('mousemove', function(event) {
       var dragResizerPos = self.get('dragResizerPos');
       self.set('resizerPos', startResizerPos + (event.clientX - startX));
       self.get('parentView').updateChildren();
     });
 
-    $(document).one('mouseup', function() {
-      $(this).off('mousemove');
+    mouseDetector.one('mouseup', function() {
+      $(this).off('mousemove').removeClass('active');
     });
 
     return false;
-  },
-
-  mouseUp: function() {
-    $(document).off('mousemove');
-  },
-
-  documentMouseMove: function(event) {
-    if (this.get('isDragging')) {
-    }
   }
 });
